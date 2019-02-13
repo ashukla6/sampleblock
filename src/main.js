795da3af980dd4c80a4ee1e9425df5e78ff7848b
RaviNayakCapgemini <47587859+RaviNayakCapgemini@users.noreply.github.com>
2019-02-13 18:49:05 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,22 @@ var SDK = require('blocksdk');
 var sdk = new SDK();
 
 
-sdk.setBlockEditorWidth("500px");
+	sdk.getContent(function (content) {
+		var quill = new Quill('#editor-container', {
+			theme: 'snow'
+		});
+		quill.root.innerHTML = content;
+		function saveText() {
+			var html = quill.root.innerHTML;
+			sdk.setContent(html);
+			sdk.setSuperContent('This is super content: ' + html);
+
+			sdk.getData(function (data) {
+				var numberOfEdits = data.numberOfEdits || 0;
+				sdk.setData({
+					numberOfEdits: numberOfEdits + 1
+				});
+			});
+		}
+		quill.on('text-change', saveText);
+	});
