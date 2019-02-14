9af0c66b93e812d416e3cc2a14fee99d41a6da1a
RaviNayakCapgemini <47587859+RaviNayakCapgemini@users.noreply.github.com>
2019-02-14 17:35:35 +0530

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -79,7 +79,7 @@ var sdk = new SDK();
 if (window.self === window.top) {
 	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
 } else {
-		sdk.setContent("<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4><h5>Heading 5</h5><h6>Heading 6</h6>");
+		sdk.setContent("<table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr><tr><td>Ernst Handel</td><td>Roland Mendel</td><td>Austria</td></tr><tr><td>Island Trading</td><td>Helen Bennett</td><td>UK</td></tr><tr><td>Laughing Bacchus Winecellars</td><td>Yoshi Tannamuri</td><td>Canada</td></tr><tr><td>Magazzini Alimentari Riuniti</td><td>Giovanni Rovelli</td><td>Italy</td></tr></table>");
 		sdk.getContent(function (content) {
 		var quill = new Quill('#editor-container', {
 			theme: 'snow'
