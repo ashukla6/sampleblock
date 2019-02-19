a74e02aba685017e0817bb576e84edc39599d222
RaviNayakCapgemini <47587859+RaviNayakCapgemini@users.noreply.github.com>
2019-02-19 14:53:53 +0530

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -81,16 +81,16 @@ var sdk = new SDK();
 if (window.self === window.top) {
 	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
 } else {
-	sdk.setContent('<p>This is a paragraph.</p>');
+	
 	sdk.getContent(function (content) {
 		var quill = new Quill('#editor-container', {
 			theme: 'snow'
 		});
 		quill.root.innerHTML = content;
 		function saveText() {
-			var html = quill.root.innerHTML();
+			var html = quill.root.innerHTML;
 			sdk.setContent(html);
-			
+			sdk.setSuperContent('This is super content: ' + html);
 
 			sdk.getData(function (data) {
 				var numberOfEdits = data.numberOfEdits || 0;
