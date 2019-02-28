3b33a4b18d73cc325a0d3b2545802898412c1fbc
RaviNayakCapgemini <47587859+RaviNayakCapgemini@users.noreply.github.com>
2019-02-28 12:33:37 +0530

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -75,7 +75,7 @@ __webpack_require__(1);
 var SDK = __webpack_require__(19);
 
 
-
+var sdk = new SDK();
 
 	
 
@@ -83,8 +83,8 @@ if (window.self === window.top) {
 	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
 } else {
 	
-	var sdk = new BlockSDK();
 	
+	sdk.setContent("<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4><h5>Heading 5</h5><h6>Heading 6</h6>");
 	sdk.getContent(function (content) {
 		var quill = new Quill('#editor-container', {
 			theme: 'snow'
